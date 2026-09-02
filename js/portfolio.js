83fa3b9b4453d7f8edef6332c28ed0794a7ddbf5
martin-mugisha <mugishamartin50@gmail.com>
2026-09-02 21:36:35 +0300

    refactor: organize portfolio assets and refresh project imagery

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,36 +1,188 @@
 (function ($) {
   "use strict";
 
-  var path = function (folder, name) {
-    return ["website pics", folder, name].map(encodeURIComponent).join("/");
-  };
-  var numbered = function (folder, prefix, numbers, extension) {
-    return numbers.map(function (number) { return path(folder, prefix + " (" + number + ")." + extension); });
-  };
-  var whatsapp = function (folder, date, times) {
-    var images = [];
-    times.forEach(function (time) {
-      ["(1)", "(2)", "(3)", "(4)", ""].forEach(function (suffix) {
-        images.push(path(folder, "WhatsApp Image " + date + " at " + time + (suffix ? " " + suffix : "") + ".jpeg"));
-      });
-    });
-    return images;
+  var basePath = "MIKO/miko";
+  var img = function (folder, filename) {
+    return [basePath, folder, filename].map(encodeURIComponent).join("/");
   };
 
   var galleries = {
-    bedroom: numbered("BEDROOM 1", "2", [1, 3, 5, 7, 8, 9], "png"),
-    cancer: numbered("CANCER INSTITUTE", "1", [1, 2, 3, 4, 6, 7, 8, 9], "png"),
-    glovo: numbered("GLOVO", "GLOVO", [1, 2, 3, 4, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25], "png"),
-    indigo: numbered("INDIGO", "INDIGO", [2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 16], "png").concat([path("INDIGO", "office.png")]),
-    kitchens: numbered("Kitchens & wadrobes", "KIGO", [2, 3, 4, 5, 7, 13], "png").concat(whatsapp("Kitchens & wadrobes", "2026-08-05", ["11.52.57"])),
-    kigo: [path("KIGO LANDSCAPE", "3d.jpeg"), path("KIGO LANDSCAPE", "ls1.jpeg")].concat(whatsapp("KIGO LANDSCAPE", "2026-07-11", ["15.48.25", "15.48.26"])),
-    blueberries: whatsapp("BLUE BERRIES HOTEL", "2026-07-11", ["15.48.18", "15.48.19", "15.48.20", "15.48.21"]),
-    eye: numbered("EYE SANTE", ".", [1, 2, 3, 4, 5], "png").concat(whatsapp("EYE SANTE", "2026-06-29", ["13.28.06", "13.28.07", "13.28.08"]), [path("EYE SANTE", "WhatsApp Image 2026-06-29 at 13.28.07 (5).jpeg")]),
-    namulanda: numbered("NAMULANDA KITCHEN", "NAMULANDA", [1, 2, 3, 4, 5], "png"),
-    tali: [path("TALI CAFE", "poertifolio.jpeg")].concat(whatsapp("TALI CAFE", "2026-07-11", ["15.48.21", "15.48.22", "15.48.23", "15.48.24"])),
-    others: [path("others", "miko.jpeg"), path("others", "cab.jpeg"), path("others", "tvu.jpeg")].concat(whatsapp("others", "2026-07-11", ["13.29.44", "13.36.26", "13.36.27", "13.36.28", "13.36.29", "15.48.24"])),
-    renders: [path("renders", "bg_1.jpeg")].concat(whatsapp("renders", "2026-06-29", ["12.34.29", "12.34.30"])),
-    sightsavers: whatsapp("sight savers", "2026-06-29", ["12.57.34"]).concat([5, 6, 7, 8, 9].map(function (number) { return path("sight savers", "WhatsApp Image 2026-06-29 at 12.57.34 (" + number + ").jpeg"); }))
+    bedroom: [
+      img("BEDROOM_1", "Bedroom_view.webp"),
+      img("BEDROOM_1", "Bed.webp"),
+      img("BEDROOM_1", "Bed_view.webp"),
+      img("BEDROOM_1", "Bed_view_with_striplight.webp"),
+      img("BEDROOM_1", "Bed_view_without_striplight.webp"),
+      img("BEDROOM_1", "Bedroom_side_view.webp"),
+      img("BEDROOM_1", "Wadrobe_view.webp")
+    ],
+    cancer: [
+      img("CANCER_INSTITUTE", "A_view_of_waiting_area.webp"),
+      img("CANCER_INSTITUTE", "Aview_of_the_furniture.webp"),
+      img("CANCER_INSTITUTE", "Aview_of_the_waiting_area.webp"),
+      img("CANCER_INSTITUTE", "Decor.webp"),
+      img("CANCER_INSTITUTE", "Furniture_view.webp"),
+      img("CANCER_INSTITUTE", "Hospital_hall_way.webp"),
+      img("CANCER_INSTITUTE", "Interior_design.webp"),
+      img("CANCER_INSTITUTE", "Other_waiting_area.webp"),
+      img("CANCER_INSTITUTE", "Reception.webp"),
+      img("CANCER_INSTITUTE", "Waiting_area.webp")
+    ],
+    glovo: [
+      img("GLOVO", "Interior_view_1.webp"),
+      img("GLOVO", "Offices_view.webp"),
+      img("GLOVO", "Offices_view_2.webp"),
+      img("GLOVO", "Work_space_1.webp"),
+      img("GLOVO", "Work_space_2.webp"),
+      img("GLOVO", "Interior_view_2.webp"),
+      img("GLOVO", "Interior_view_3.webp"),
+      img("GLOVO", "Room_1.webp"),
+      img("GLOVO", "Room_2.webp"),
+      img("GLOVO", "Sitting_area.webp"),
+      img("GLOVO", "Waiting_area.webp"),
+      img("GLOVO", "Dining_space.webp"),
+      img("GLOVO", "Kitchen_view.webp"),
+      img("GLOVO", "Inner_view.webp"),
+      img("GLOVO", "Interior_design.webp"),
+      img("GLOVO", "Furniture.webp"),
+      img("GLOVO", "GLOVO_(9).webp"),
+      img("GLOVO", "Bag_storage_space_1.webp"),
+      img("GLOVO", "bag_storage_space_2.webp")
+    ],
+    indigo: [
+      img("INDIGO", "Interior_view.webp"),
+      img("INDIGO", "Dining_&_Kitchen_view.webp"),
+      img("INDIGO", "Dining_view.webp"),
+      img("INDIGO", "Interior.webp"),
+      img("INDIGO", "Kitchen_side_1.webp"),
+      img("INDIGO", "Meeting_room_view_1.webp"),
+      img("INDIGO", "Meeting_room_view_2.webp"),
+      img("INDIGO", "meeting_room_3.webp"),
+      img("INDIGO", "Reception.webp"),
+      img("INDIGO", "View_of_work_space_in_night.webp"),
+      img("INDIGO", "View_of_workspace_in_day.webp")
+    ],
+    kitchens: [
+      img("KITCHEN_&_WADROBES", "Kitchen_view_1.webp"),
+      img("KITCHEN_&_WADROBES", "Kitchen_view_2.webp"),
+      img("KITCHEN_&_WADROBES", "Kitchen_view_3.webp"),
+      img("KITCHEN_&_WADROBES", "Wadrobe_1.webp"),
+      img("KITCHEN_&_WADROBES", "Wadrobe_2.webp"),
+      img("KITCHEN_&_WADROBES", "Wadrobe_3.webp"),
+      img("KITCHEN_&_WADROBES", "Wadrobe_4.webp")
+    ],
+    kigo: [
+      img("KIGO_LANDSCAPE", "Landscape_view.webp"),
+      img("KIGO_LANDSCAPE", "Front_pool_view.webp"),
+      img("KIGO_LANDSCAPE", "Back_pool_view.webp"),
+      img("KIGO_LANDSCAPE", "Compound_view.webp"),
+      img("KIGO_LANDSCAPE", "Flower_garden.webp"),
+      img("KIGO_LANDSCAPE", "Grid_view.webp"),
+      img("KIGO_LANDSCAPE", "Night_view.webp"),
+      img("KIGO_LANDSCAPE", "Parking_view.webp"),
+      img("KIGO_LANDSCAPE", "Side_view.webp")
+    ],
+    blueberries: [
+      img("BLUE_BERRIES_HOTEL", "Dining_view.webp"),
+      img("BLUE_BERRIES_HOTEL", "Area_view.webp"),
+      img("BLUE_BERRIES_HOTEL", "Back_view.webp"),
+      img("BLUE_BERRIES_HOTEL", "Bedroom_view.webp"),
+      img("BLUE_BERRIES_HOTEL", "Class_hold_view.webp"),
+      img("BLUE_BERRIES_HOTEL", "Counter_front_view.webp"),
+      img("BLUE_BERRIES_HOTEL", "Counter_view.webp"),
+      img("BLUE_BERRIES_HOTEL", "Dinner.webp"),
+      img("BLUE_BERRIES_HOTEL", "Dinner_2.webp"),
+      img("BLUE_BERRIES_HOTEL", "Front_counter_view_with_lights.webp"),
+      img("BLUE_BERRIES_HOTEL", "Hall_way.webp"),
+      img("BLUE_BERRIES_HOTEL", "Interior_view.webp"),
+      img("BLUE_BERRIES_HOTEL", "Logo.webp"),
+      img("BLUE_BERRIES_HOTEL", "Outside_view.webp"),
+      img("BLUE_BERRIES_HOTEL", "TV_Unit_view.webp"),
+      img("BLUE_BERRIES_HOTEL", "Wall_unit.webp")
+    ],
+    eye: [
+      img("EYE_SANTE", "Interior_setup_1.webp"),
+      img("EYE_SANTE", "Area_view_1.webp"),
+      img("EYE_SANTE", "Area_view_2.webp"),
+      img("EYE_SANTE", "Area_view_3.webp"),
+      img("EYE_SANTE", "Company_Logo.webp"),
+      img("EYE_SANTE", "Company_logo_2.webp"),
+      img("EYE_SANTE", "Hall_way.webp"),
+      img("EYE_SANTE", "Side_view_of_sitting_area.webp"),
+      img("EYE_SANTE", "Sitting_area_1.webp"),
+      img("EYE_SANTE", "Sitting_area_2.webp"),
+      img("EYE_SANTE", "Waiting_area_1.webp"),
+      img("EYE_SANTE", "Waiting_area_2.webp")
+    ],
+    namulanda: [
+      img("NAMULANDA", "Kitchen_view.webp"),
+      img("NAMULANDA", "Dining_view.webp"),
+      img("NAMULANDA", "Kitchen_view_with_lights.webp"),
+      img("NAMULANDA", "Kitchen_view_without_lights.webp")
+    ],
+    tali: [
+      img("TALI_CAFE", "Dining_area_1.webp"),
+      img("TALI_CAFE", "Dining_area_2.webp"),
+      img("TALI_CAFE", "Dining_area_3.webp"),
+      img("TALI_CAFE", "Dining_area_4.webp"),
+      img("TALI_CAFE", "Dining_area_5.webp"),
+      img("TALI_CAFE", "Dining_area_6.webp"),
+      img("TALI_CAFE", "Counter.webp"),
+      img("TALI_CAFE", "Handwashing_area_1.webp"),
+      img("TALI_CAFE", "Handwashing_area_2.webp"),
+      img("TALI_CAFE", "Inside_view.webp"),
+      img("TALI_CAFE", "Outside_view.webp"),
+      img("TALI_CAFE", "Sitting_area.webp"),
+      img("TALI_CAFE", "View_of_the_dining_area.webp")
+    ],
+    sightsavers: [
+      img("SITE_SAVERS", "Interior_view.webp"),
+      img("SITE_SAVERS", "Company_name_logo.webp"),
+      img("SITE_SAVERS", "Consultation_area.webp"),
+      img("SITE_SAVERS", "Decor.webp"),
+      img("SITE_SAVERS", "Inside_view.webp"),
+      img("SITE_SAVERS", "Other_interior_view.webp"),
+      img("SITE_SAVERS", "Waiting_area_1.webp"),
+      img("SITE_SAVERS", "Waiting_area_2.webp")
+    ],
+    hospital: [
+      img("ST_JOSEPH_HOSPITAL_-_WAKISO", "A_view_of_sitting_area_&_Counter.webp"),
+      img("ST_JOSEPH_HOSPITAL_-_WAKISO", "Reception_area.webp"),
+      img("ST_JOSEPH_HOSPITAL_-_WAKISO", "Reception_display.webp"),
+      img("ST_JOSEPH_HOSPITAL_-_WAKISO", "Waiting_area.webp"),
+      img("ST_JOSEPH_HOSPITAL_-_WAKISO", "View_of_a_Waiting_station.webp"),
+      img("ST_JOSEPH_HOSPITAL_-_WAKISO", "Sitting_area.webp"),
+      img("ST_JOSEPH_HOSPITAL_-_WAKISO", "Hall_way_view.webp"),
+      img("ST_JOSEPH_HOSPITAL_-_WAKISO", "Private_ward.webp"),
+      img("ST_JOSEPH_HOSPITAL_-_WAKISO", "Overview_of_private_ward.webp"),
+      img("ST_JOSEPH_HOSPITAL_-_WAKISO", "Patient's_ward.webp"),
+      img("ST_JOSEPH_HOSPITAL_-_WAKISO", "Patients'_beds.webp"),
+      img("ST_JOSEPH_HOSPITAL_-_WAKISO", "Matenaty_ward.webp"),
+      img("ST_JOSEPH_HOSPITAL_-_WAKISO", "Kitchen_area.webp")
+    ],
+    residential_more: [
+      img("MORE_RESIDENTIAL_SPACES", "Dining_space.webp"),
+      img("MORE_RESIDENTIAL_SPACES", "Dining_view.webp"),
+      img("MORE_RESIDENTIAL_SPACES", "Kitchen_space.webp"),
+      img("MORE_RESIDENTIAL_SPACES", "Kitchen_view.webp"),
+      img("MORE_RESIDENTIAL_SPACES", "TV_Unit.webp"),
+      img("MORE_RESIDENTIAL_SPACES", "TV_Unit_side_view.webp"),
+      img("MORE_RESIDENTIAL_SPACES", "TV_Unit_view.webp"),
+      img("RESIDENTIALS", "Cabinet_view.webp"),
+      img("RESIDENTIALS", "interior_view.webp")
+    ],
+    renders: [
+      img("RENDERS", "Whole_space_view.webp"),
+      img("RENDERS", "Dining_view.webp"),
+      img("RENDERS", "Kitchen_full_view.webp"),
+      img("RENDERS", "Kitchen_side_view.webp")
+    ],
+    others: [
+      img("MORE_RESIDENTIAL_SPACES", "Dining_space.webp"),
+      img("MORE_RESIDENTIAL_SPACES", "TV_Unit_view.webp"),
+      img("RESIDENTIALS", "Cabinet_view.webp"),
+      img("RESIDENTIALS", "interior_view.webp")
+    ]
   };
 
   $(".project-gallery-trigger").on("click", function (event) {
